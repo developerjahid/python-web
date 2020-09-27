7bbc10491d6c11ac7f2048b5c2acf5cbe63c83e9
Developer Jahid <jhpabel@gmail.com>
2020-09-27 20:08:49 +0600

    for/while loop

--- app.py
+++ app.py
@@ -2,4 +2,25 @@ import turtle
 
 jahid_turtle = turtle.Turtle()
 
-print(jahid_turtle.forward(100))
+def square():
+    print(jahid_turtle.forward(100))
+    print(jahid_turtle.right(90))
+    print(jahid_turtle.forward(100))
+    print(jahid_turtle.right(90))
+    print(jahid_turtle.forward(100))
+    print(jahid_turtle.right(90))
+    print(jahid_turtle.forward(100))
+
+# square()
+# print(jahid_turtle.forward(200))
+# square()
+
+# jahid = "happy"
+
+# while jahid == "happy" :
+#     square()
+
+for count in range(4):
+    square()
+
+
